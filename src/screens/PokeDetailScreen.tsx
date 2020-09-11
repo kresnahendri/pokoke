584509fc7a78af07e88453ffe5e82c483d3d2825
kresnahendri <kresna@ruangguru.com>
2020-09-11 22:27:01 +0700

    Layouting poke detail screen

--- src/screens/PokeDetailScreen.tsx
+++ src/screens/PokeDetailScreen.tsx
@@ -1,7 +1,11 @@
+/* eslint-disable react/no-array-index-key */
 import React from "react";
-import {Text} from "react-native";
+import {ActivityIndicator, Image, ScrollView, View} from "react-native";
+import {SvgUri} from "react-native-svg";
 
 import Container from "../components/Container";
+import Spacer from "../components/Spacer";
+import Text from "../components/Text";
 import {useGetPokeDetail} from "../hooks/http/poke/useGetPokeDetail";
 import {
   PokeDetailNavigation,
@@ -14,12 +18,57 @@ interface Props {
 }
 
 const PokeDetailScreen: React.FC<Props> = ({route}) => {
-  const {data} = useGetPokeDetail({name: route.params.name});
+  const {data, isLoading} = useGetPokeDetail({name: route.params.name});
 
-  // TODO: use as component's data provider
-  console.log(data);
+  if (!data || isLoading) {
+    return <ActivityIndicator />;
+  }
 
-  return <Container>{data && <Text>{data.name}</Text>}</Container>;
+  const {
+    back_default,
+    back_shiny,
+    front_default,
+    front_shiny,
+    other,
+  } = data.sprites;
+  const image = other.dream_world.front_default || "";
+
+  return (
+    <ScrollView style={{paddingHorizontal: 16, flex: 1}}>
+      <Container>
+        <View style={{flex: 1, alignItems: "center"}}>
+          <Text variant="title" value={data.name} />
+          <SvgUri width={175} height={175} uri={image} />
+          <Spacer height={16} />
+          <View style={{flexDirection: "row"}}>
+            {[back_default, back_shiny, front_default, front_shiny].map(
+              (thumbnail, i) => (
+                <Image
+                  key={i}
+                  source={{uri: thumbnail}}
+                  style={{width: 70, height: 70}}
+                />
+              ),
+            )}
+          </View>
+        </View>
+        <Spacer height={24} />
+        <View>
+          <Text weight="bold" value="Moves" />
+          <Spacer height={8} />
+          <View style={{flexDirection: "row", flexWrap: "wrap"}}>
+            {data.moves.map(({move}) => (
+              <Text
+                value={move.name}
+                key={move.name}
+                style={{marginRight: 10}}
+              />
+            ))}
+          </View>
+        </View>
+      </Container>
+    </ScrollView>
+  );
 };
 
 export default PokeDetailScreen;
